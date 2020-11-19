a5216666e6743c204272d11b05dbf651cd7c05e9
Romain Clair <romain.clair@wildcodeschool.fr>
2020-11-19 11:50:38 +0100

    Wipe out solution

--- src/CashMachine.php
+++ src/CashMachine.php
@@ -6,19 +6,4 @@
 
 declare(strict_types=1);
 
-class CashMachine
-{
-    public const BILLS = [500, 200, 100, 50, 20, 10, 5, 2, 1];
-    public static function getChange(int $amount): array
-    {
-        $result = [];
-        foreach(self::BILLS as $bill) {
-            $number = intdiv($amount,$bill);
-            if ($number > 0) {
-                $result[$bill] = $number;
-            }
-            $amount %= $bill;
-        }
-        return $result;
-    }
-}
+// TODO : A CashMachine class with a getChange class method
